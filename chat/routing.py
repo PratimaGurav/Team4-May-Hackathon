37f4cccd5ed9d1c6411651959bcbaedf02206347
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-05-19 21:17:41 +0800

    Add basic websocket functionality

--- chat/routing.py
+++ chat/routing.py
@@ -3,5 +3,5 @@ from .consumers import ChatConsumer
 
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>[^/]+)/$', ChatConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<room_name>\w+)/$', ChatConsumer.as_asgi()),
 ]
--- chat/views.py
+++ chat/views.py
@@ -4,4 +4,6 @@ from django.views import View
 
 class chatView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'chat/chat.html', {})
+        return render(request, 'chat/chat.html', {
+            'chat_room': 'helloAlex'
+        })
