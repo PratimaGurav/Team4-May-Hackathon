56d27f7d38676b9280438f7a375bbe530ebf4ed5
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-05-19 13:00:34 +0800

    Change project settings

--- group_chatting/settings.py
+++ group_chatting/settings.py
@@ -19,14 +19,11 @@ if os.path.exists('env.py'):
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
+# Secret key set in the env.py
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Debu set in the env.py
+DEBUG = os.environ['DEBUG'] == 'True'
 
 ALLOWED_HOSTS = []
 
