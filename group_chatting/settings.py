f1914f9fedcf199b93e19fda0c6d53f14811655a
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-05-19 14:29:17 +0800

    Add if statement for the databases

--- group_chatting/settings.py
+++ group_chatting/settings.py
@@ -73,12 +73,20 @@ WSGI_APPLICATION = 'group_chatting.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES
+if "DATABASE_URL" in os.environ:
+    print("database = PostgreSQL via Heroku")
+    DATABASES = {
+        # "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
-}
+else:
+    print("database = db.sqlite3")
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
