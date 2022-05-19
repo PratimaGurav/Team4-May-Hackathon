38be7d059367a3ad5a9960f053b18096a68079e3
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-05-19 14:35:11 +0800

    Add allauth core settings

--- group_chatting/settings.py
+++ group_chatting/settings.py
@@ -72,6 +72,23 @@ TEMPLATES = [
 ]
 
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+
 WSGI_APPLICATION = 'group_chatting.wsgi.application'
 
 
