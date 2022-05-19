5784aa00c048e5d4aeaf51d3f16472057db4776e
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-05-19 19:12:57 +0800

    Add chat and messages models to chat app

---
+++ chat/consumers.py
@@ -0,0 +1,2 @@
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Chat(models.Model):
+    """Chat model"""
+    chat_name = models.CharField(max_length=100)
+    chat_description = models.CharField(max_length=250)
+    members = models.ManyToManyField(User, related_name='chats')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.chat_name
+
+    class Meta:
+        """Ordering chats by created_at"""
+        ordering = ['-created_at']
+
+
+class Message(models.Model):
+    """Message model"""
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.message
+
+    class Meta:
+        """ Order messages by creation date. """
+        ordering = ['-created_at']
