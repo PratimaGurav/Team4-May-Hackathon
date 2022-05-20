6be4d270a20f0c51a4b8f70ea884d0dc8ac2bc33
lexach91 <ronmctailor@protonmail.com>
2022-05-20 16:32:40 +0800

    Remove creating of chat_messages variable from chat/views.py

--- chat/views.py
+++ chat/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 from django.views import View
-from .models import Chat, Message
+from .models import Chat, ChatMessage
 
 
 class ChatRoomView(View):
     def get(self, request, *args, **kwargs):
         chat = Chat.objects.get(slug=kwargs['slug'])
-        messages = Message.objects.filter(chat=chat).order_by('-created_at')
         return render(
             request,
             'chat/chat_room.html',
