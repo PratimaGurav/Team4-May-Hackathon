657cf96619fe218563116f0ce6728f1502d5f3ae
lexach91 <ronmctailor@protonmail.com>
2022-05-20 16:31:32 +0800

    Add code to send an image over a websocket in javascript

--- static/js/chat_room.js
+++ static/js/chat_room.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', function () {
   const roomName = window.location.pathname.split('/')[2];
   const chatContainer = document.getElementsByClassName('chat-container')[0];
   const chatInput = document.getElementById('chat-input');
+  const fileInput = document.getElementById('chat-file');
+  const sendButton = document.getElementById('chat-send');
   const socketProtocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
   const socketUrl = `${socketProtocol}//${window.location.host}/ws/chat/${roomName}/`;
   const chatSocket = new WebSocket(socketUrl);
@@ -22,30 +24,76 @@ document.addEventListener('DOMContentLoaded', function () {
   chatSocket.onmessage = function (e) {
     let data = JSON.parse(e.data);
     console.log(data);
+    let messageContent = data.message;
+    let messageUsername = data.username;
+    let messageImageUrl = data.image;
+
+    let message = document.createElement('div');
+    message.classList.add('message');
+
+    let messageUsernameElement = document.createElement('span');
+    messageUsernameElement.classList.add('message-username');
+    messageUsernameElement.innerText = messageUsername;
+    message.appendChild(messageUsernameElement);
+
+    let messageContentElement = document.createElement('span');
+    messageContentElement.innerText = messageContent;
+    message.appendChild(messageContentElement);
+
+    if (messageImageUrl) {
+      let messageImage = document.createElement('img');
+      messageImage.classList.add('message-image');
+      messageImage.src = messageImageUrl;
+      message.appendChild(messageImage);
+    }
+
+    chatContainer.appendChild(message);
+    chatContainer.scrollTop = chatContainer.scrollHeight;
    
-    let messageHtml = `<div class="message">
-    <div class="message-header">
-                <div class="message-header-username">
-                    ${data.username}
-                </div>
-                <div class="message-header-time">
-                    ${new Date().toLocaleTimeString()}
-                </div>
-            </div>
-            <div class="message-body">
-                ${data.message}
-            </div>
-        </div>`;
-    chatContainer.innerHTML += messageHtml;
   };
 
-  document.addEventListener('keydown', function (e) {
-    if(e.key == 'Enter') {
-      console.log('Enter pressed');
+  // document.addEventListener('keydown', function (e) {
+  //   if(e.key == 'Enter') {
+  //     console.log('Enter pressed');
+  //     const chatMessage = {
+  //       'chat_id': chatId,
+  //       'message': chatInput.value,
+  //       'username': username
+  //     };
+  //     chatSocket.send(JSON.stringify(chatMessage));
+  //     chatInput.value = '';
+  //   }
+  // });
+
+  sendButton.addEventListener('click', function (e) {
+    // need to check if file is selected
+    if (fileInput.files.length > 0) {
+      console.log('File selected');
+      // we need to temporarily store the file in the local storage
+      // so that we can send its url to the consumer over the socket
+      // and then we can remove it from the local storage
+      const file = fileInput.files[0];
+      const fileReader = new FileReader(file);
+      fileReader.readAsDataURL(file);
+      fileReader.onload = function (e) {
+        // console.log(e.target.result);
+        const fileUrl = e.target.result;
+        const chatMessage = {
+          'chat_id': chatId,
+          'message': chatInput.value,
+          'username': username,
+          'image': fileUrl
+        };
+        chatSocket.send(JSON.stringify(chatMessage));
+        fileInput.value = '';
+        chatInput.value = '';
+      };
+    } else {
       const chatMessage = {
         'chat_id': chatId,
         'message': chatInput.value,
-        'username': "Anonymous"
+        'username': username,
+        'image': null
       };
       chatSocket.send(JSON.stringify(chatMessage));
       chatInput.value = '';
@@ -53,4 +101,4 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
 
-})
+});
