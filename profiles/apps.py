b8b5854550f6c23abe7cafb402c9097585d0dce0
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-05-22 14:10:15 +0800

    Fix signals for the profiles

--- profiles/apps.py
+++ profiles/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_save
 
 
 class ProfilesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'profiles'
+    def ready(self):
+        from profiles.signals import create_profile
+        from django.contrib.auth.models import User
+        post_save.connect(create_profile, sender=User)
+        from profiles.signals import save_profile
+        post_save.connect(save_profile, sender=User)
