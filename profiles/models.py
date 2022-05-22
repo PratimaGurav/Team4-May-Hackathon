446a71e6e4bb39102996e1bc5b4aaf790e5a8207
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-05-22 15:35:22 +0800

    Fix code in profiles app

--- profiles/models.py
+++ profiles/models.py
@@ -1,15 +1,14 @@
+"""Models for profiles app."""
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
 
-# Create your models here.
-
-
-# Creating profile model by extending the user model
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_profile')
+    """Profile model"""
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE,
+        related_name='user_profile'
+    )
     avatar = CloudinaryField(folder='profile_images')
     stewardship = models.ManyToManyField(User, blank=True)
     bio = models.TextField()
@@ -24,14 +23,3 @@ class Profile(models.Model):
         if self.avatar:
             return self.avatar.url
         return '/media/profile_images/avatar.png'
-
-
-# @receiver(post_save, sender=User)
-# def create_profile(sender, instance, created, **kwargs):
-#     if created:
-#         Profile.objects.create(user=instance)
-
-
-# @receiver(post_save, sender=User)
-# def save_profile(sender, instance, **kwargs):
-#     instance.user_profile.save()
--- profiles/signals.py
+++ profiles/signals.py
@@ -1,3 +1,4 @@
+"""Signals for the profiles app."""
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from profiles.models import Profile
