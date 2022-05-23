c2343ce30dc9eb3f5e755750648f7ac2227682d2
lexach91 <ronmctailor@protonmail.com>
2022-05-23 14:15:53 +0800

    Change order of commands in asgi.py

--- group_chatting/asgi.py
+++ group_chatting/asgi.py
@@ -8,14 +8,15 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'group_chatting.settings')
+
+from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from django.core.asgi import get_asgi_application
 import chat.routing
-django_asgi_app = get_asgi_application()
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'group_chatting.settings')
 
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
