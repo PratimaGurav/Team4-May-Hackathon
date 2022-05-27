49963b6e3db7d090deff06c70de542fdcef03230
PratimaGurav <prtmgrv2@gmail.com>
2022-05-27 16:28:45 +0000

    Create modeels.py and modify forms.py

--- home/views.py
+++ home/views.py
@@ -38,10 +38,11 @@ class HomeView(View):
 class ContactView(FormView):
     template_name = 'contact-us.html'
     form_class = ContactForm
-    success_url = '/contact/'
+    # success_url = '/contact/'
 
     def form_valid(self, form):
         # This method is called when valid form data has been POSTed.
         # It should return an HttpResponse.
         form.send_email()
         return super().form_valid(form)
+    
